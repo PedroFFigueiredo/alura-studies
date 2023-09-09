4c6fed764fe0e7ff1f8dbb6ac3258d3805731fda
PedroFFigueiredo <pedrohenrifagundesfigueiredo@gmail.com>
2023-09-09 18:07:07 -0300

    Criando novo componente Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Button from './components/Button';
+import Form from './components/Form/index';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Button />
+      <Form />
     </div>
   );
 }
