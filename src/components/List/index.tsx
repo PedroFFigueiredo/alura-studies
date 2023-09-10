3ac9aa4d4c92e0cea8c205d45fe3b791825febe6
PedroFFigueiredo <pedrohenrifagundesfigueiredo@gmail.com>
2023-09-09 21:15:14 -0300

    Adicionando funcionalidade de adicioanar um card

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './List.module.scss'
 import Item from './Item';
 
 function List() {
-  const tasks = [{
+  const [tasks, setTasks] = useState([{
     task: 'React',
     time: '02:00:00'
   },
@@ -15,10 +15,12 @@ function List() {
     task: 'Typescript',
     time: '03:00:00'
   }
-  ]
+  ])
   return (
     <aside className={styles.taksList}>
-      <h2>Estudos do dia</h2>
+      <h2 onClick={() => {
+        setTasks([...tasks, { task: 'Estudar estado', time: '05:00:00' }])
+      }}>Estudos do dia</h2>
       <ul>
         {tasks.map((data, index) => (
           <Item
